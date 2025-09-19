108fc32ce63d9dd838e4fa25012c42f4636970f1
fn2926 <fn2926@MSS2175-21>
2025-09-19 13:53:05 -0400

    push for creation of scripts folder, input folder

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour 
+{
+    public Camera mainCam;
+
+    public GameObject followedObj;
+
+    public
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+
+    }
+
+}
