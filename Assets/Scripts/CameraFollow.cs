f9df87795fbca4ab20e25a0bfa135af591dd219c
fn2926 <fn2926@MSS2175-21>
2025-09-22 13:44:32 -0400

    push for camera changes

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -9,12 +9,13 @@ public class CameraFollow : MonoBehaviour
 
     private void Start()
     {
-        
+        mainCam.transform.Rotate(new Vector3(45, 0, 0));
     }
 
     private void Update()
     {
-
+        mainCam.transform.position = new Vector3(followedObj.transform.position.x - 5, followedObj.transform.position.y + 5, followedObj.transform.position.z - 20);
+        
     }
 
 }
