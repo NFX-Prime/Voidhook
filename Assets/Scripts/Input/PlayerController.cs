315d8544ea0021dcd3376759a719b0ff90d28c24
James Alfred Manansala <jrm2043@rit.edu>
2025-09-30 15:07:39 -0400

    Trying lots of movement scripting

---
+++ Assets/Scripts/Input/PlayerController.cs
@@ -0,0 +1,74 @@
+﻿using KBCore.Refs;
+using UnityEngine;
+
+namespace Platformer
+{
+    public class PlayerController : ValidatedMonoBehaviour
+    {
+        [Header("References")]
+        [SerializeField, Self] CharacterController controller;
+        [SerializeField, Self] Animator animator;
+        // Cinemachine doesn't work and we don't really need it right now.
+        // [SerializeField, Anywhere] CinemachineFreeLook freeLookVCam;
+        [SerializeField, Anywhere] InputReader input;
+
+        [Header("Settings")]
+        [SerializeField] float moveSpeed = 6f;
+        [SerializeField] float rotationSpeed = 15f;
+        [SerializeField] float smoothTime = 0.2f;
+
+        Transform mainCam;
+
+        float currentSpeed;
+        float velocity;
+
+        const float ZeroF = 0f;
+
+        private void Awake() {
+            mainCam = Camera.main.transform;
+
+
+        }
+
+        private void Update()
+        {
+            HandleMovement();
+            // Handle UpdateAnimator(); later
+        }
+
+        void HandleMovement()
+        {
+            var movementDirection = new Vector3(input.Direction.x, y: 0f, z: input.Direction.y).normalized;
+
+            var adjustedDirection = Quaternion.AngleAxis(mainCam.eulerAngles.y, Vector3.up) * movementDirection;
+
+
+            var adjustedMovement = movementDirection * moveSpeed * Time.deltaTime;
+
+            if (adjustedDirection.magnitude > ZeroF)
+
+            HandleRotation(adjustedDirection);
+            controller.Move(adjustedMovement);
+
+            currentSpeed = Mathf.SmoothDamp(currentSpeed, adjustedMovement.magnitude, ref velocity, smoothTime);
+
+        }
+
+        float SmoothSpeed(float value)
+        {
+            return Mathf.SmoothDamp(currentSpeed, value, ref velocity, smoothTime);
+        }
+        
+        void HandleRotation(Vector3 adjustedDirection)
+        {
+            // Adjust rotation to match movement direction
+            var targetRotation = Quaternion.LookRotation(adjustedDirection);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+            transform.LookAt(transform.position + adjustedDirection);
+        }
+
+        
+
+    }
+
+}
--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -15,7 +15,7 @@ public class Movement : MonoBehaviour
 
     private CharacterController controller;
     private Vector3 playerVelocity;
-    private bool groundedPlayer;
+    public bool groundedPlayer;
 
     [Header("Input Actions")]
     public InputActionReference moveAction; // expects Vector2
@@ -48,27 +48,20 @@ public class Movement : MonoBehaviour
             doubleJump = false;
         }
 
-        // Read input
+        // Read input only if in joint in the
         Vector2 input = moveAction.action.ReadValue<Vector2>();
         Vector3 move = new Vector3(input.x, 0, input.y);
         move = Vector3.ClampMagnitude(move, 1f);
 
-        if (move != Vector3.zero)
+
+        // 
+        if (move != Vector3.zero && groundedPlayer == true)
         {
             transform.forward = move;
         }
 
-        // Jump
-        if (jumpAction.action.triggered && groundedPlayer)
-        {
-            playerVelocity.y = Mathf.Sqrt(jumpHeight * -2.0f * gravityValue);
-        }
-        else if (jumpAction.action.triggered && doubleJump == false)
-        {
-            playerVelocity.y = Mathf.Sqrt(jumpHeight * -2.0f * gravityValue);
-            // Prevents another double jump to happen.
-            doubleJump = true;
-        }
+        // Calling the jump function
+        jumpFunction();
 
 
         // Apply gravity
@@ -77,5 +70,26 @@ public class Movement : MonoBehaviour
         // Combine horizontal and vertical movement
         Vector3 finalMove = (move * playerSpeed) + (playerVelocity.y * Vector3.up);
         controller.Move(finalMove * Time.deltaTime);
+
+        
+    }
+
+
+    void jumpFunction()
+    {
+        // Jump
+        if (jumpAction.action.triggered && groundedPlayer)
+        {
+            playerVelocity.y = Mathf.Sqrt(jumpHeight * -2.0f * gravityValue);
+            groundedPlayer = controller.isGrounded;
+
+        }
+        else if (jumpAction.action.triggered && doubleJump == false)
+        {
+            playerVelocity.y = Mathf.Sqrt(jumpHeight * -2.0f * gravityValue);
+            // Prevents another double jump to happen.
+            doubleJump = true;
+            groundedPlayer = controller.isGrounded;
+        }
     }
 }
