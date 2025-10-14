981fea0540662e850784c2e6bba939dcebb6521a
iboxp <iboxp@FILIBERTO>
2025-10-14 18:05:55 -0400

    push for boss ai changes + boss chasing player

--- Assets/Scripts/EnemyAI/BossAI.cs
+++ Assets/Scripts/EnemyAI/BossAI.cs
@@ -9,12 +9,16 @@ public class BossAI : MonoBehaviour
     public TargetSwap[] targetSwaps = new TargetSwap[4];
     public TargetSwap targetCurrent;
     public TargetSwap targetNext;
+
+    public Transform player;
     // Can change how fast enemy is
     public float chaseSpeed = 3f;
     // Speed when player is idle
     public float slowSpeed = 1.5f;
     // How close before it stops
     public float stopDistance = 1.5f;
+
+    public float distance;
 
     public int numtargets = 0;
 
@@ -36,25 +40,34 @@ public class BossAI : MonoBehaviour
         if (targets == null || targetSwaps == null) return;
 
         // Always chase the player, but adjust speed based on walking/idle
+        distance = Vector3.Distance(player.position, transform.position);
 
-        for(int i = 0; i < targetSwaps.Length; i++)
+        if (distance < 15.0f)
         {
-            if (targetSwaps[i].targetted)
-            {
-                
-                float distance = Vector3.Distance(transform.position, targets[i].position);
-                if (distance > stopDistance)
-                {
-                    agent.SetDestination(targets[i].position);
-                }
-                else
-                {
-                    // stop when close enough (TEMPORARY)
-                    agent.isStopped = true;
-                }
-            }
-
+            agent.SetDestination(player.position);
         }
+        else
+        {
+            for (int i = 0; i < targetSwaps.Length; i++)
+            {
+                if (targetSwaps[i].targetted)
+                {
+
+                    float distance = Vector3.Distance(transform.position, targets[i].position);
+                    if (distance > stopDistance)
+                    {
+                        agent.SetDestination(targets[i].position);
+                    }
+                    else
+                    {
+                        // stop when close enough (TEMPORARY)
+                        agent.isStopped = true;
+                    }
+                }
+
+            }
+        }
+
     }
 
     public void SwitchTarget()
