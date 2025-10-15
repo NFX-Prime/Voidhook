40df6f7270a09b4b09cc1b15c63b42a87b422e6b
WillRIT <144167180+WillRIT@users.noreply.github.com>
2025-10-14 22:40:55 -0400

    Pause almost done
    needs to be able to pause but you can unpause

---
+++ Assets/Pause.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Pause : MonoBehaviour
+{
+    //Bottom Sheet Group
+    private VisualElement _bottomContainer;
+    //Open Button
+
+    //Close Button
+    private Button _closeButton;
+    //Bottom Sheet
+    private VisualElement _bottomSheet;
+    //Scrim
+    private VisualElement _scrim;
+
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Grab the topmost visual element in the UI Document
+        var root = GetComponent<UIDocument>().rootVisualElement;
+
+
+        //Close Button
+        _closeButton = root.Q<Button>("Button_Close");
+
+        //Registering buttons' callback methods
+        _closeButton.RegisterCallback<ClickEvent>(OnCloseButtonClicked);
+    }
+
+    private void OnCloseButtonClicked(ClickEvent evt)
+    {
+        this.gameObject.SetActive(false);
+    }
+    
+    
+
+}
