864b682d999ed922bfbf59206c4cd440745c400d
fn2926 <fn2926@MSS2175-21>
2025-10-29 13:53:18 -0400

    push for minor scene changes + added rigid/collider to new player model

--- Assets/Scripts/EnemyAI/PlayerFall.cs
+++ Assets/Scripts/EnemyAI/PlayerFall.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class PlayerFall : MonoBehaviour
 {
     public Transform player;
-    private void OnCollisionEnter(Collision collision)
+    private void OnTriggerEnter(Collider other)
     {
         player.position = new Vector3(-33.7f, 5.41f, 37.88f);
     }
