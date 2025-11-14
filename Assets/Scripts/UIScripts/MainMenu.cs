9b2eb6347366e05d94f9bdef197429a007b0a162
James Alfred Rodrigazo Manansala <135179209+DaJMan185@users.noreply.github.com>
2025-11-14 06:51:56 -0500

    Intro video implemented when player presses on start button

--- Assets/Scripts/UIScripts/MainMenu.cs
+++ Assets/Scripts/UIScripts/MainMenu.cs
@@ -1,7 +1,16 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
 
 public class MainMenu : MonoBehaviour
 {
+    // Set the videoplayer object (this holds the video itself)
+    public VideoPlayer videoPlayer;
+    // Set what scene goes after the video is played
+    public string nextSceneName;
+    // Assign canvas so that we can hide it when the video is playing
+    public Canvas mainCanvas;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -20,4 +29,29 @@ public class MainMenu : MonoBehaviour
         UnityEngine.SceneManagement.SceneManager.LoadScene("November 15th Build");    
         Debug.Log("Game Started");
     }
+
+    // Called by the Start button
+    public void PlayCutscene()
+    {
+        // Simply disabling the canvas when the game starts.
+        if (mainCanvas != null)
+        {
+            mainCanvas.enabled = false;
+        }
+
+        // Make sure the VideoPlayer is active
+        videoPlayer.gameObject.SetActive(true); 
+        videoPlayer.Play();
+        // Event when video ends
+        videoPlayer.loopPointReached += OnVideoFinished; 
+    }
+
+    private void OnVideoFinished(VideoPlayer vp)
+    {
+        // Remove listener
+        videoPlayer.loopPointReached -= OnVideoFinished;
+        // Load game scene
+        SceneManager.LoadScene(nextSceneName); 
+    }
+
 }
