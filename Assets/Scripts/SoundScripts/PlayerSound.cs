dc2f4033f88d4e1a33c8372e0617f34e46f97db8
James Alfred Rodrigazo Manansala <135179209+DaJMan185@users.noreply.github.com>
2025-12-05 12:29:04 -0500

    Attempting to fix walking sound

--- Assets/Scripts/SoundScripts/PlayerSound.cs
+++ Assets/Scripts/SoundScripts/PlayerSound.cs
@@ -2,15 +2,27 @@ using UnityEngine;
 
 public class PlayerSound : MonoBehaviour
 {
+    public AudioClip[] playerSounds;
+
+    private AudioSource source;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        source = GetComponent<AudioSource>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void PlayWalkingSound()
     {
+        // Play the walking sound
+        AudioClip clip = playerSounds[2];
+        source.clip = clip;
+        source.volume = Random.Range(0.02f, 0.05f);
+        source.pitch = Random.Range(0.8f, 1.2f);
+        source.Play();
+        Debug.Log(clip.name);
+
+
         
     }
 }
